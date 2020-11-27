7614aa8cb9d9a8e7c775c7260669ffacba55ec70
Johan Flensmark <jokf@itu.dk>
2020-11-27 14:02:56 +0100

    added address data to testdatagenerator

--- QueueSafe.Models.Test/TestDataGenerator.cs
+++ QueueSafe.Models.Test/TestDataGenerator.cs
@@ -17,7 +17,8 @@ namespace QueueSafe.Models.Test
                 {
                     new Booking { TimeStamp = DateTime.Now, State = Active, Token = "goodtoken1" },
                     new Booking { TimeStamp = DateTime.Now, State = Pending, Token = "goodtoken2" }
-                }
+                },
+                Address = new Address { StreetName = "Skidtvej", HouseNumber = 20, Postal = 3300}
             };
 
             var ElMinidik = new Store
@@ -29,7 +30,8 @@ namespace QueueSafe.Models.Test
                     new Booking { TimeStamp = DateTime.Now, State = Active, Token = "goodtoken3" },
                     new Booking { TimeStamp = DateTime.Now, State = Pending, Token = "goodtoken4" },
                     new Booking { TimeStamp = DateTime.Now, State = Canceled, Token = "goodtoken5" }
-                }
+                },
+                Address = new Address { StreetName = "Godvej", HouseNumber = 10, Postal = 1000}
             };
 
             context.Store.AddRange(GaldalfsButtHashing, ElMinidik);
